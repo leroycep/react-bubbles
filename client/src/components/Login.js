cf2b7773be48e98f2c89deef25c9f988f1a1d068
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-27 09:48:44 -0600

    Make login form; store token in localStorage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
+import axios from "axios";
+
+import { BASE_API, TOKEN_STORAGE_KEY } from "../constants";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const usernameField = useRef();
+  const passwordField = useRef();
+
+  const handleLogin = ev => {
+    ev.preventDefault();
+    const username = usernameField.current.value;
+    const password = passwordField.current.value;
+    axios.post(`${BASE_API}/login`, { username, password }).then(res => {
+      window.localStorage.setItem(TOKEN_STORAGE_KEY, res.data.payload);
+    });
+  };
+
   return (
-    <>
+    <form onSubmit={handleLogin}>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <label>
+        Username: <input name="username" type="text" ref={usernameField} />
+      </label>
+      <br />
+      <label>
+        Password: <input name="password" type="password" ref={passwordField} />
+      </label>
+      <input type="submit" />
+    </form>
   );
 };
 
---
+++ client/src/constants.js
@@ -0,0 +1,2 @@
+export const TOKEN_STORAGE_KEY = "token";
+export const BASE_API = "http://localhost:5000/api";
